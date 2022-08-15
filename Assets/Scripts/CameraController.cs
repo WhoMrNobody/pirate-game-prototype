e00e0dea1139b6932d693f7b055dfb034300d0eb
WhoMrNobody <the.emrehun@gmail.com>
2022-08-15 22:15:50 +0300

    Added the new UI PNG Files and FreeLook System

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,54 +1,78 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using Cinemachine;
 
-public class CameraController : MonoBehaviour
-{
-   
-    private bool _dragPanMoveActive=false;
-    private Vector2 _lastMousePos;
-    private Vector3 inputDir = new Vector3(0, 0, 0);
-    void Update()
+public class CameraController : MonoBehaviour, IPointerDownHandler, IPointerExitHandler
+ {
+    public float TouchSensitivity_x = 10f;
+    public float TouchSensitivity_y = 10f;
+ 
+    private int touchID;
+    private bool onPad;
+ 
+    private void Start()
     {
-        
-
-        if(Input.GetMouseButtonDown(1)){
-            _dragPanMoveActive = true;
-            _lastMousePos = Input.mousePosition;
-        }
-        if(Input.GetMouseButtonUp(1)){
-            _dragPanMoveActive = false;
-        }
-
-        if(_dragPanMoveActive){
-
-            Vector2 mouseMovementDelta = (Vector2)Input.mousePosition - _lastMousePos;
-
-            float dragPanSpeed = 2f;
-            inputDir.x = mouseMovementDelta.x * dragPanSpeed;
-            inputDir.z = mouseMovementDelta.y * dragPanSpeed;
-
-            _lastMousePos = Input.mousePosition;
-        }
-
-        HandleCameraRotation();
-
-        HandleCameraMovement();
+        CinemachineCore.GetInputAxis = HandleAxisInputDelegate;
     }
+ 
+    float HandleAxisInputDelegate(string axisName)
+    {
+        if (onPad)
+        {
+             switch (axisName)
+             {
+                 case "Mouse X":
+                     if (Input.touchCount > 0)
+                     {
+                         return Input.touches[touchID].deltaPosition.x / TouchSensitivity_x;
+                     }
+                     else
+                     {
+                         return Input.GetAxis(axisName);
+                     }
+ 
+                 case "Mouse Y":
+                     if (Input.touchCount > 0)
+                     {
+                         return Input.touches[touchID].deltaPosition.y / TouchSensitivity_y;
+                     }
+                     else
+                     {
+                         return Input.GetAxis(axisName);
+                     }
+ 
+                 default:
+                     Debug.LogError("Input <" + axisName + "> not recognyzed.", this);
+                     break;
+             }
+         }
+         return 0f;
+     }
+ 
+     public void OnPointerExit(PointerEventData eventData)
+     {
+        onPad = false;
 
-    private void HandleCameraRotation(){
+     }
+     public void OnPointerDown(PointerEventData eventData)
+     {
+         if (Input.touchCount > 0)
+         {
+             foreach (Touch touch in Input.touches)
+             {
+                 int id = touch.fingerId;
+                 if (touch.phase == TouchPhase.Began && EventSystem.current.IsPointerOverGameObject(id))
+                 {
+                    onPad = true;
+                    touchID = id;
+ 
+                    break;
+                 }
+             }
+         }
+     }
+     
 
-        float rotateDir = 0f;
-        if(Input.GetKey(KeyCode.Q)) rotateDir = +1;
-        if(Input.GetKey(KeyCode.E)) rotateDir = -1f;
-
-        float rotateSpeed = 50f;
-        transform.eulerAngles += new Vector3(0, rotateDir * rotateSpeed * Time.deltaTime, 0);
-    }
-
-    private void HandleCameraMovement(){
-
-        
-
-    }
 }
